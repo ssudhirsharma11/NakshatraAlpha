5bb72aea864cebd668bf6345c6994eb1073823b4
ssudhirsharma11 <ssudhirsharma11@gmail.com>
2026-07-09 21:58:54 +0530

    summary
    Created project structure

---
+++ src/main.py
@@ -0,0 +1,19 @@
+"""
+NakshatraAlpha
+Financial Astrology Research Platform
+
+Author : Sudhir Sharma
+Version : 0.1.0
+"""
+
+
+def main():
+    print("=" * 50)
+    print("        NakshatraAlpha")
+    print("Financial Astrology Research Platform")
+    print("Version 0.1.0")
+    print("=" * 50)
+
+
+if __name__ == "__main__":
+    main()
